e46ef8facfcc57dc841ee770bd3381f8eb88a83f
lzszygg <1924309603@qq.com>
2021-08-28 20:24:53 +0800

    第二次上传，添加了页面

---
+++ mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+<<<<<<< HEAD
+
+=======
+<<<<<<< HEAD
+
+=======
+#include"begin.h"
+>>>>>>> 1b956ca (第二次上传，添加了一个页面)
+>>>>>>> 9ffc591 (第二次上传，添加了页面)
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+    begin page;
+>>>>>>> 1b956ca (第二次上传，添加了一个页面)
+>>>>>>> 9ffc591 (第二次上传，添加了页面)
+};
+
+#endif // MAINWINDOW_H
